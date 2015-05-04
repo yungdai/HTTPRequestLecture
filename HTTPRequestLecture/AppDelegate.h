c0a33eca85dbfd6d4c1da76e20ae18852a1f0b71
Yung Dai <yungchidai@gmail.com>
2015-05-04 10:23:57 -0400

    Initial Commit

---
+++ HTTPRequestLecture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPRequestLecture
+//
+//  Created by Yung Dai on 2015-05-04.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPRequestLecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPRequestLecture
+//
+//  Created by Yung Dai on 2015-05-04.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
